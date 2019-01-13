76721f896122122037d45f4f41afa848d0ae12e5
aDreamCatcher <344944410@qq.com>
2019-01-14 00:30:46 +0800

    worked

---
+++ BlinkingLabel.swift
@@ -0,0 +1,8 @@
+//
+//  BlinkingLabel.swift
+//  BlinkingLabel
+//
+//  Created by lgy on 2019/1/13.
+//
+
+import Foundation
--- Example/BlinkingLabel/ViewController.swift
+++ Example/BlinkingLabel/ViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import BlinkingLabel // This Module was unrecognized many time. (solution: clear Xcode cache)
+                     // modify BlinkingLabel module's code need clear cache.(may need set buildSetting)
 
 class ViewController: UIViewController {
 
+    // MARK: properties
+
+    var isBlinking = false
+    let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10,
+                                                 y: 120,
+                                                 width: 200,
+                                                 height: 30))
+
+    // MARK: life cycle methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Setup the BlinkingLabel
+        blinkingLabel.text = "I Blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+
+        view.addSubview(blinkingLabel)
+        isBlinking = true
+
+        // Create a UIButton to toggle the blinking
+        let toggleButton = UIButton(type: .custom)
+        toggleButton.frame = CGRect(x: 10,
+                                    y: 160,
+                                    width: 125,
+                                    height: 30)
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(UIColor.red, for: .normal)
+        toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
+
+        view.addSubview(toggleButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MAKR: actions
+
+    @objc func toggleBlinking() {
+        if isBlinking {
+            blinkingLabel.stopBlinking()
+        } else {
+            blinkingLabel.startBlinking()
+        }
+
+        isBlinking = !isBlinking
     }
 
 }
